0c1dea944117acfd4d1a720811c5f8d8d6ce3f58
Himanshu Jangid <himanshujhar@gmail.com>
2024-03-26 09:50:53 +0530

    updated autodelete and readme

--- test/auto-delete.js
+++ test/auto-delete.js
@@ -1,14 +1,13 @@
-import * as fs from "fs";
-import * as path from "path";
-import { promisify } from "util";
-
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util");
 const readdir = promisify(fs.readdir);
 const stat = promisify(fs.stat);
 const unlink = promisify(fs.unlink);
 
 async function deleteFilesOlderThan14Days(folderPaths) {
   const currentDate = new Date();
-  currentDate.setDate(currentDate.getDate() - 14);
+  currentDate.setDate(currentDate.getDate() - 20);
 
   for (const folderPath of folderPaths) {
     try {
@@ -16,8 +15,7 @@ async function deleteFilesOlderThan14Days(folderPaths) {
       for (const file of files) {
         const filePath = path.join(folderPath, file);
         const fileStats = await stat(filePath);
-
-        if (fileStats.isFile() && fileStats.ctime < currentDate) {
+        if (fileStats.isFile() && fileStats.birthtime < currentDate) {
           await unlink(filePath);
           console.log(`Deleted file: ${filePath}`);
         }
@@ -28,6 +26,13 @@ async function deleteFilesOlderThan14Days(folderPaths) {
   }
 }
 
-// Usage example
-const folderPaths = ["/home/himanshu/Documents/bds/bds"];
+// const folderPaths = [path.resolve(process.cwd(), './images/input'), path.resolve(process.cwd(), './images/output')]
+const folderPaths = [
+  "C:\\Users\\Wisflux Dev\\Documents\\images\\output",
+  "C:\\Users\\Wisflux Dev\\Documents\\images\\input",
+];
+
+console.log(folderPaths);
+
+console.log("Deleting files from the following directories", folderPaths);
 deleteFilesOlderThan14Days(folderPaths);
